27b96d25f7e07267d5f6189cbaeae6340fe05688
Camilo Sanchez <camilosanchezdev@gmail.com>
2021-08-30 22:49:43 -0300

    Fix error uuid package

--- backend-ys/src/index.js
+++ backend-ys/src/index.js
@@ -1,70 +1,71 @@
-const express = require('express');
-var cors = require('cors')
-require('dotenv').config()
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const path = require('path');
-const multer = require('multer')
-const uuid = require('uuid/dist/v4')
-const {format} = require('timeago.js')//TODO
-const http = require('http');
-const socketIO = require('socket.io') 
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const path = require("path");
+const multer = require("multer");
+const { format } = require("timeago.js"); //TODO
+const http = require("http");
+const socketIO = require("socket.io");
 
 //Initializations
-const app= express();
-app.use(cors())
-require('dotenv').config({ path: 'variables.env' });
+const app = express();
+app.use(cors());
+require("dotenv").config({ path: "variables.env" });
 
 //Server is listenning
-const server = http.createServer(app)
+const server = http.createServer(app);
 const port = process.env.PORT || 4000;
-server.listen(4000, ()=>{console.log('Server on port', {port});})
+server.listen(4000, () => {
+  console.log("Server on port", { port });
+});
 
 // IO = esta es la comunicacion del backend
 module.exports.io = socketIO(server);
-require('./sockets/socket');
+require("./sockets/socket");
 
 //Middleware
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //app.use(methodOverride('_method'));
-app.use(session({
-    secret:'mysecretapp',
-    resave:true,
-    saveUninitialized:true
-}));
+app.use(
+  session({
+    secret: "mysecretapp",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use((req, res, next)=>{
-    app.locals.format = format; 
-    next();
-})
+app.use((req, res, next) => {
+  app.locals.format = format;
+  next();
+});
 
-//Static Files 
-app.set('public', path.join(__dirname, 'public'))
-app.use(express.static(path.join(__dirname, 'public')));
+//Static Files
+app.set("public", path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
-app.use(require('./routers/localidadRoute'));
-app.use(require('./routers/tipoArticuloRoute'));
-app.use(require('./routers/articuloRoute'));
-app.use(require('./routers/configuracionRoute'));
-app.use(require('./routers/comprobanteDetalleRoute'));
-app.use(require('./routers/comprobanteRoute'));
-app.use(require('./routers/clienteRoute'));
-app.use(require('./routers/authRoute'));
-app.use(require('./routers/boxRoute'));
-app.use(require('./routers/branchRoute'));
-app.use(require('./routers/offerRoute'));
+app.use(require("./routers/localidadRoute"));
+app.use(require("./routers/tipoArticuloRoute"));
+app.use(require("./routers/articuloRoute"));
+app.use(require("./routers/configuracionRoute"));
+app.use(require("./routers/comprobanteDetalleRoute"));
+app.use(require("./routers/comprobanteRoute"));
+app.use(require("./routers/clienteRoute"));
+app.use(require("./routers/authRoute"));
+app.use(require("./routers/boxRoute"));
+app.use(require("./routers/branchRoute"));
+app.use(require("./routers/offerRoute"));
 
-mongoose.connect(process.env.DATABASECONNETION,{
-
+mongoose
+  .connect(process.env.DATABASECONNETION, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
-})
-.then(db => console.log('DB is connected'))
-.catch(err => console.error(err)); 
-
-
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log("DB is connected"))
+  .catch((err) => console.error(err));
