55752794485e4452bcf8af6566b94c94e4a4d324
Camilo Sanchez <camilosanchezdev@gmail.com>
2021-10-06 00:03:57 -0300

    List of articles

--- mobile-ys/src/app/pages/home/home.module.ts
+++ mobile-ys/src/app/pages/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { IonicModule} from '@ionic/angular'
+import { IonicModule } from '@ionic/angular'
 import { FormsModule } from '@angular/forms'
 import { HomePage } from './home.page'
 import { HomePageRoutingModule } from './home-routing.module'
+import { ArticleApi } from 'src/app/shared/api/article.api'
 
 @NgModule({
-    
     imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
     declarations: [HomePage],
+    providers: [ArticleApi],
 })
 export class HomePageModule {}
--- mobile-ys/src/app/pages/home/home.page.ts
+++ mobile-ys/src/app/pages/home/home.page.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { MenuController } from '@ionic/angular'
-import { Article } from 'src/app/shared/models/articleModel';
-import { ArticleService } from 'src/app/shared/services/articlesService/article.service';
-import { environment } from 'src/environments/environment';
+import { Subscription } from 'rxjs'
+import { ArticleApi } from 'src/app/shared/api/article.api'
+import { ArticleModel } from 'src/app/shared/models/article.model'
+import { environment } from 'src/environments/environment'
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-    articles: Article[];
-    image_Path : string;
+export class HomePage implements OnInit, OnDestroy {
+    private subscriptions = new Subscription()
+    articles: ArticleModel[]
+    image_Path: string
 
-    constructor(private menu: MenuController, private articleService: ArticleService) { 
-
-        this.image_Path = environment.HOST_API;
+    constructor(private menu: MenuController, private articleApi: ArticleApi) {
+        this.image_Path = environment.HOST_API
+    }
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe()
     }
 
-    togglemenu = () =>{
-        this.menu.toggle();
+    togglemenu = () => {
+        this.menu.toggle()
     }
 
-    ngOnInit(){
-        this.articleService.getAll().subscribe(
-            res =>{
-                this.articles = Object.values(res)[1];
-            },
-            error=> console.log(error)
+    ngOnInit() {
+        this.getArticles()
+    }
+    getArticles(): void {
+        this.subscriptions.add(
+            this.articleApi.all().subscribe({
+                error: (error: any) => {
+                    console.error(error)
+                },
+                next: (articles) => {
+                    this.articles = articles
+                },
+            })
         )
     }
 }
